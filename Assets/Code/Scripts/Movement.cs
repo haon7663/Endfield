771741a68686b535e5d429aea74b357fc01e0b9f
haon <89632611+haon7663@users.noreply.github.com>
2024-10-07 23:42:04 +0900

    update : Movement

--- Assets/Code/Scripts/Movement.cs
+++ Assets/Code/Scripts/Movement.cs
@@ -1,13 +1,53 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    
     private Unit _unit;
+    private bool _isMove;
 
     private void Start()
     {
         _unit = GetComponent<Unit>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            OnMove(GridManager.Tiles[_unit.Tile.Point + Vector2.left]);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            OnMove(GridManager.Tiles[_unit.Tile.Point + Vector2.right]);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            OnMove(GridManager.Tiles[_unit.Tile.Point + Vector2.down]);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            OnMove(GridManager.Tiles[_unit.Tile.Point + Vector2.up]);
+        }
+    }
+
+    public void OnMove(Tile tile)
+    {
+        if (_isMove) return;
+        
+        var sequence = DOTween.Sequence();
+
+        _isMove = true;
+        sequence.Append(transform.DOMove(tile.transform.position + Vector3.up * 0.5f, 0.25f))
+            .Join(spriteRenderer.transform.DOLocalJump(Vector3.zero, 0.25f, 1, 0.3f))
+            .OnComplete(() =>
+            {
+                _unit.Place(tile);
+                _isMove = false;
+            });
     }
 }
