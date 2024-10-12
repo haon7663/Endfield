35d3fb6ad06b5242a686b1e4958fc3d8263ce1fb
haon <89632611+haon7663@users.noreply.github.com>
2024-10-12 21:02:55 +0900

    update : AIController

--- Assets/Code/Scripts/Tactical/Units/Component/AIController.cs
+++ Assets/Code/Scripts/Tactical/Units/Component/AIController.cs
@@ -13,6 +13,14 @@ public class AIController : MonoBehaviour
 
     private void Update()
     {
-        _unit.Tile.GetAreaInRange(2);
+        EnemyMove();
+    }
+    
+    private void EnemyMove() // 적 -> 플레이어쪽으로 이동
+    {
+        var target = GameManager.Inst.player;
+        float direction = target.position.x - transform.position.x;
+        int targetTileIndex = direction > 0 ? _unit.Tile.Key + 1 : _unit.Tile.Key - 1;
+        _movement.OnMove(GridManager.Inst.GetTile(targetTileIndex));
     }
 }
