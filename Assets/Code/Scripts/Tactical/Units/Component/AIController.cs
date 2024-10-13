c24533d951a12f251303118696db7318b8aafdea
Oriatbf <john31534249@gmail.com>
2024-10-13 22:22:38 +0900

    Hover AI

--- Assets/Code/Scripts/Tactical/Units/Component/AIController.cs
+++ Assets/Code/Scripts/Tactical/Units/Component/AIController.cs
@@ -23,22 +23,35 @@ public class AIController : MonoBehaviour
         var target = GameManager.Inst.player;
         float direction = target.transform.position.x - transform.position.x;
         Debug.Log(direction);
-        bool isRight = direction > 0 ? true : false; //타겟이 오른쪽에 있는지
+        bool targetIsRight = direction > 0 ? true : false; //타겟이 오른쪽에 있는지
+        bool tileIsRight;
         int max = -100, min = 100;
 
 
         List<Tile> areaTile = target.Tile.GetAreaInRange(2);
-        foreach(Tile tile in areaTile) if (tile.Key == _unit.Tile.Key) return; //이미 범위 안에 있다면 리턴 및 스킬 시전
+       // List<Tile> areaTile = target.Tile.GetTilesOnRange(2);
+        foreach (Tile tile in areaTile) if (tile.Key == _unit.Tile.Key) return; //이미 범위 안에 있다면 리턴 및 스킬 시전
 
-        for(var i = 0;i< areaTile.Count;i++)
+        for(var i = 0;i< areaTile.Count;i++)  //가장 높은 값과 가장 낮은 값을 찾아냄
         {
             if (areaTile[i].Key>max) max= areaTile[i].Key;
             else if (areaTile[i].Key<min) min= areaTile[i].Key;
         }
 
-        if (isRight) _movement.OnMove(GridManager.Inst.GetTile(_unit.Tile.Key+1));
+        if (targetIsRight) //멀리 가려는 AI
+        {
+            if (min > _unit.Tile.Key) tileIsRight = true;
+            else tileIsRight= false;
+        }
+        else
+        {
+            if (max > _unit.Tile.Key) tileIsRight = true;
+            else tileIsRight = false;
+        }
+
+        if (tileIsRight) _movement.OnMove(GridManager.Inst.GetTile(_unit.Tile.Key+1));
         else _movement.OnMove(GridManager.Inst.GetTile(_unit.Tile.Key - 1));
-        _movement.OnFlip(!isRight);
+        _movement.OnFlip(!targetIsRight);
 
     }
 }
