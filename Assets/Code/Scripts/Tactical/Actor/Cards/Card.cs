5b9243db031fa7b659831a5b45d45e4314ddae5c
haon <89632611+haon7663@users.noreply.github.com>
2024-10-20 23:58:19 +0900

    update : Card Rotation

--- Assets/Code/Scripts/Tactical/Actor/Cards/Card.cs
+++ Assets/Code/Scripts/Tactical/Actor/Cards/Card.cs
@@ -6,30 +6,25 @@ using UnityEngine.UI;
 
 public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private RectTransform rectTransform;
     [SerializeField] private Image backGround;
+
+    private float _rotateValue;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        backGround.transform.DOLocalRotateQuaternion(Quaternion.identity, 0.5f).SetEase(Ease.OutCirc);
+        DOVirtual.Float(_rotateValue, 0, 0.5f, value => _rotateValue = value).SetEase(Ease.OutBack);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        backGround.transform.DOLocalRotateQuaternion(Quaternion.Euler(24, 24, 8f), 0.5f).SetEase(Ease.OutCirc);
+        DOVirtual.Float(_rotateValue, -25f, 0.5f, value => _rotateValue = value).SetEase(Ease.OutBack);
     }
-    
-    public float rotationSpeed = 30f; // 회전 속도
 
-    // void Update()
-    // {
-    //     var point1 = transform.position 
-    //     // 빨간 선의 방향 벡터 계산
-    //     Vector3 axis = (point2.position - point1.position).normalized;
-    //
-    //     // 선의 중간 지점을 회전 중심으로 설정
-    //     Vector3 center = (point1.position + point2.position) / 2;
-    //
-    //     // 해당 축과 중심을 기준으로 회전
-    //     transform.RotateAround(center, axis, rotationSpeed * Time.deltaTime);
-    // }
+    private void Update()
+    {
+        var axis = rectTransform.sizeDelta;
+        var rotation = Quaternion.AngleAxis(_rotateValue, axis);
+        transform.rotation = rotation;
+    }
 }
