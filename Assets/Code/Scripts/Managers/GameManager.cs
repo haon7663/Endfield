3d45dc47f105336c6e8fab6d5b2cb8c2406b43df
Oriatbf <john31534249@gmail.com>
2024-10-20 22:57:34 +0900

    BSVolume

--- Assets/Code/Scripts/Managers/GameManager.cs
+++ Assets/Code/Scripts/Managers/GameManager.cs
@@ -1,16 +1,31 @@
+using NUnit.Framework;
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class GameManager : Singleton<GameManager>
 {
     public Unit player;
+    [SerializeField] private Volume globalVolume;
+    [SerializeField] private ColorAdjustments colorAdjustments;
 
     public int maxElixir;
     public float curElixir;
+    private void Start()
+    {
+        if (globalVolume.profile.TryGet<ColorAdjustments>(out colorAdjustments)) { }
+    }
 
     private void Update()
     {
         curElixir += Time.deltaTime;
         curElixir = Mathf.Clamp(curElixir, 0, maxElixir);
+    }
+
+    public void BSVolume(bool inSimulation)
+    {  
+        colorAdjustments.saturation.value = inSimulation? -100:0;
     }
 }
