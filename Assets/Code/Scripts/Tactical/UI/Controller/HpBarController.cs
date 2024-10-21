8b986376b8c00a2af335e4df22ac5a7e6f82c544
NAKOBERRY <tt01033135063@gmail.com>
2024-10-21 20:12:22 +0900

    Revert "Update : HpViewer, HpBarController"
    This reverts commit c73a4ea0acd62d0639486b31f9effb6eb95043bc, reversing
    changes made to 46be8bb02912f2b647068edd0869c4847391ccaf.

--- Assets/Code/Scripts/Tactical/UI/Controller/HpBarController.cs
+++ Assets/Code/Scripts/Tactical/UI/Controller/HpBarController.cs
@@ -1,26 +1,12 @@
 using UnityEngine;
-using System.Collections.Generic;
 
 public class HpBarController : MonoBehaviour
 {
     public Camera mainCamera;
+    public Transform targetObject;
 
-    public List<Transform> targetObjects = new List<Transform>();
-    public List<Transform> healthTexts = new List<Transform>();
-
-    private void Update()
+    void Update()
     {
-        for (int i = 0; i < targetObjects.Count; i++)
-        {
-            Vector3 screenPosition = mainCamera.WorldToScreenPoint(targetObjects[i].position);
-
-            healthTexts[i].position = screenPosition;
-        }
-    }
-
-    public void AddHpBar(Transform targetObject, Transform healthText)
-    {
-        targetObjects.Add(targetObject);
-        healthTexts.Add(healthText);
+        Vector3 screenPosition = mainCamera.WorldToScreenPoint(targetObject.position);
     }
 }
