fad46197fe5f1684a98c7e4301cc415b67485712
NAKOBERRY <tt01033135063@gmail.com>
2024-10-21 19:55:17 +0900

    Update : hpviewer,HpBarController

--- Assets/Code/Scripts/Tactical/UI/Controller/HpBarController.cs
+++ Assets/Code/Scripts/Tactical/UI/Controller/HpBarController.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class HpBarController : MonoBehaviour
 {
     public Camera mainCamera;
-    public Transform targetObject;
 
-    void Update()
+    public List<Transform> targetObjects = new List<Transform>();
+    public List<Transform> healthTexts = new List<Transform>();
+
+    private void Update()
     {
-        Vector3 screenPosition = mainCamera.WorldToScreenPoint(targetObject.position);
+        for (int i = 0; i < targetObjects.Count; i++)
+        {
+            Vector3 screenPosition = mainCamera.WorldToScreenPoint(targetObjects[i].position);
+
+            healthTexts[i].position = screenPosition;
+        }
+    }
+
+    public void AddHpBar(Transform targetObject, Transform healthText)
+    {
+        targetObjects.Add(targetObject);
+        healthTexts.Add(healthText);
     }
 }
