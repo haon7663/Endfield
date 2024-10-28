da23fb0dea34c5e91c01a09c25bf9d1bee045126
NAKOBERRY <tt01033135063@gmail.com>
2024-10-28 23:00:27 +0900

    Add : DefeatPanelController

---
+++ Assets/Code/Scripts/Tactical/UI/Controller/DefeatPanelController.cs
@@ -0,0 +1,22 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+
+public class DefeatPanelController : MonoBehaviour
+{
+    [SerializeField] private Panel panel;
+    [SerializeField] private ClosePanel closePanel;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            panel.SetPosition(PanelStates.Show, true, 0.5f, Ease.OutBack);
+            closePanel.onClose += Hide;
+        }
+    }
+    public void Hide()
+    {
+        panel.SetPosition(PanelStates.Hide, true, 0.25f);
+    }
+}
