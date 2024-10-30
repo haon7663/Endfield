d215cb0e07a7dda89cc58ba27a3ac76ef8e211cd
NAKOBERRY <tt01033135063@gmail.com>
2024-10-31 00:20:45 +0900

    Update : defeatPanelController

--- Assets/Code/Scripts/Tactical/UI/Controller/DefeatPanelController.cs
+++ Assets/Code/Scripts/Tactical/UI/Controller/DefeatPanelController.cs
@@ -11,6 +11,7 @@ public class DefeatPanelController : MonoBehaviour
     [SerializeField] private TMP_Text elitePlantKill;
     [SerializeField] private TMP_Text gainedSkill;
     [SerializeField] private TMP_Text gainedArtifact;
+    [SerializeField] private InventoryController inventoryController;
 
 
     private void Update()
