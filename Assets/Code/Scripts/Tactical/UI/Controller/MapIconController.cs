52e4fed4fdcfdabf5e1ce252f588169609ba9231
Oriatbf <john31534249@gmail.com>
2024-11-05 21:18:49 +0900

    MapIconController

--- Assets/Code/Scripts/Tactical/UI/Controller/MapIconController.cs
+++ Assets/Code/Scripts/Tactical/UI/Controller/MapIconController.cs
@@ -113,6 +113,12 @@ public class MapIconController : MonoBehaviour
     public void MoveNextMap()
     {
         _curMapIndex = DataManager.Inst.Data.stageCount;
+        if (_curMapIndex >= mapSequence.Count)
+        {
+            Debug.Log("게임 끝");
+            //게임끝 함수
+        }
+        
         for (int i = 0; i < _curMapIndex; i++)
         {
             _mapIcons[i].SetMapState(MapState.FinishMap);
