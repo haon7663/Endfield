b39cc3621deeb76a098395c52daf080ab61a99bb
Oriatbf <john31534249@gmail.com>
2024-11-06 11:53:26 +0900

    shopRelic

---
+++ Assets/Code/Scripts/Tactical/UI/ChestEvent/GetRelic_ChestEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GetRelic_ChestEvent : ChestEvent
+{
+    public override (Sprite, string) Excute()
+    {
+
+        //유물 이름, 스프이트 가져오기
+        iconName = "";
+
+        return (SkillLoader.GetSkillSprite(iconName), iconName);
+    }
+}
---
+++ Assets/Code/Scripts/Tactical/UI/Shop/ShopRelic.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ShopRelic : ShopItem
+{
+    protected override void Start()
+    {
+        base.Start();
+
+    }
+
+    public void RandomRelicInput()
+    {
+
+    }
+
+    protected override void BuyAction()
+    {
+        //유물 얻는 스크립트
+    }
+  
+}
