03fb8035a9a56c14250684efd29b6e49f1b1e2da
NAKOBERRY <165394329+NAKOBERRY@users.noreply.github.com>
2024-11-07 10:13:40 +0900

    Update : ArtifactManger

--- Assets/Code/Scripts/Managers/ArtifactManager.cs
+++ Assets/Code/Scripts/Managers/ArtifactManager.cs
@@ -35,7 +35,6 @@ public class ArtifactManager : SingletonDontDestroyOnLoad<ArtifactManager>
 
     public void ArtifactForStage() 
     {
-        ResetMaxHp();
         HpRegen();
         BonusGoldOnClear();
         GetSkillUpgradeTicket();
